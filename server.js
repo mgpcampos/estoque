97e37d6c93ad0a8651d78cd635040446eb3cda59
Mateus <109235896+mgpcampos@users.noreply.github.com>
2025-11-26 21:45:19 +0000

    Atualiza estrutura do projeto e configuração do banco de dados

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const app = require("./src/app");
-const sequelize = require("./src/config/database");
 
 const PORT = 3000;
 
-sequelize.sync().then(() => {
+app.db.sequelize.sync().then(() => {
 	app.listen(PORT, () =>
 		console.log(`Aplicação ativa em: http://localhost:${PORT}`),
 	);
--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const path = require("node:path");
+const methodOverride = require("method-override");
+
 const app = express();
 
-const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
-
-const path = require("node:path");
 app.use(express.static(path.join(__dirname, "views")));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (_req, res) => {
+	res.send("<h1>Hello, World!</h1>");
+});
 
 module.exports = app;
