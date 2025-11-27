d0ef43b520c88a367c9046ea3a92a56561205379
mgpcampos <109235896+mgpcampos@users.noreply.github.com>
2025-11-27 12:30:41 -0300

    Configura express-session e middleware global de usuário

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 const express = require("express");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const path = require("node:path");
+
 const app = express();
 
-const methodOverride = require("method-override");
+// Configuração do method-override para PUT e DELETE
 app.use(methodOverride("_method"));
 
-const path = require("node:path");
-app.use(express.static(path.join(__dirname, "views")));
+// Arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
+// Parser de body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configuração da view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Configuração da sessão
+app.use(session({
+	secret: 'chave-secreta-do-sistema-de-estoque',
+	resave: false,
+	saveUninitialized: false
+}));
+
+// Middleware global para injetar usuário nas views
+app.use((req, res, next) => {
+	res.locals.user = req.session.userId || null;
+	next();
+});
+
+// Rotas
+app.use(require('./routes/authRoutes'));
+app.use(require('./routes/indexRoutes'));
+app.use(require('./routes/userRoutes'));
+app.use(require('./routes/categoryRoutes'));
+app.use(require('./routes/productRoutes'));
 
 module.exports = app;
