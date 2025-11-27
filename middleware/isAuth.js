f8ceb0464b8400d2e7367e6dafd2d09852a73ba3
mgpcampos <109235896+mgpcampos@users.noreply.github.com>
2025-11-27 12:31:08 -0300

    Implementa middleware de autenticação isAuth

---
+++ middleware/isAuth.js
@@ -0,0 +1,11 @@
+// Middleware de autenticação
+// Verifica se o usuário está logado antes de acessar rotas protegidas
+
+function isAuth(req, res, next) {
+	if (req.session.userId) {
+		return next();
+	}
+	return res.redirect('/registrar');
+}
+
+module.exports = isAuth;
